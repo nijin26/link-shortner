d5e44e99e7f7b066e291d6e2b3407d91ca336862
Nijin-26 <nijinazarkpz@gmail.com>
2021-03-21 14:43:30 +0530

    Auth Page Initialized

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 
+import { FaGoogle, FaGithub, FaTwitter } from "react-icons/all";
+import authImage from "../../assets/auth.png";
+
 import styles from "./Auth.module.css";
 
 const Auth = () => {
   return (
     <div className={styles.container}>
-      <img src="" alt="logo" />
-      <div className={styles.social}></div>
-      <form></form>
+      <img src={authImage} alt="logo" />
+      <p>Login With Social</p>
+      <div className={styles.social}>
+        <FaGoogle size={28} />
+        <FaTwitter size={28} />
+        <FaGithub size={28} />
+      </div>
+      <form className={styles.form}>
+        <label htmlFor="name">Username:</label>
+        <input type="text" name="username" id="name" />
+        <label htmlFor="email">Email:</label>
+        <input type="mai" name="email" id="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" />
+        <label htmlFor="confirm">Confirm Password:</label>
+        <input type="password" name="confirm" id="confirm" />
+        <button type="submit"> Sign Up </button>
+      </form>
     </div>
   );
 };
