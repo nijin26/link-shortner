e3b0c0bc0dbc528bae53a9e586ed6ce12f9d438d
nijin26 <nijinazarkpz@gmail.com>
2021-04-06 23:13:42 +0530

    Navbar styled initiated

---
+++ src/styles/components/NavBar.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AppNavBar = styled.nav`
+  background: dodgerblue;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const image = styled.div``;
