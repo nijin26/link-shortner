cecf9b7215487e3e2b1446c9bb371f9dbe3ba3fa
Nijin-26 <nijinazarkpz@gmail.com>
2021-04-08 13:12:23 +0530

    Link Shortening Endpoint added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import QRCodeGen from "../../Components/QRCodeGen/QRCodeGen";
 import Share from "../../Components/Share/Share";
@@ -9,11 +10,20 @@ import styles from "./Home.module.css";
 const Home = () => {
   const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
+  const [result, setResult] = useState("");
   const [isShowing, setIsShowing] = useState(false);
+
+  const api = "https://secret-castle-86507.herokuapp.com/api/v1/shorten_url";
 
   const onShorten = (e) => {
     e.preventDefault();
-    setIsShowing(true);
+    axios
+      .post(api, { user_url: url })
+      .then((res) => {
+        setIsShowing(true);
+        setResult(res.data.data.shortened_url);
+      })
+      .catch((err) => console.log(err));
   };
 
   const closeModalHandler = () => {
@@ -22,7 +32,7 @@ const Home = () => {
 
   return (
     <div className={styles.container}>
-      <form>
+      <form onSubmit={onShorten}>
         <input
           value={title}
           onChange={(e) => setTitle(e.target.value)}
@@ -35,15 +45,15 @@ const Home = () => {
           onChange={(e) => setUrl(e.target.value)}
           placeholder="Loooong URL"
         />
-        <button onClick={onShorten} disabled={url === ""}>
+        <button type="submit" disabled={url === ""}>
           Shorten
         </button>
       </form>
       <Modal show={isShowing} close={closeModalHandler}>
         <div className={styles.resultModal}>
-          <QRCodeGen target={url} />
-          <input type="text" value={url} />
-          <Share target={url} />
+          <QRCodeGen target={result} />
+          <input type="text" value={result} />
+          <Share target={result} />
         </div>
       </Modal>
     </div>
