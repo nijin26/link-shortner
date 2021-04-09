67b662d5e1e2ee08c6a40bc980d88c527e79cd44
Nijin-26 <nijinazarkpz@gmail.com>
2021-04-09 23:53:04 +0530

    Global style updated

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -4,6 +4,18 @@ const globalStyle = createGlobalStyle`
     html{
         font-size: 62.5%;
     }
+
+    body{
+        margin:0;
+        padding:0;
+    }
+
+    main {
+        display: flex;
+        flex-direction: column;
+         min-height: 87.3vh;
+    }
+
 `;
 
 export default globalStyle;
